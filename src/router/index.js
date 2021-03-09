2f0d38bb5d24ba07f719e3ea47b80a3acd54bafe
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-03-09 01:08:11 -0300

    animacoes com o velocity ao entrar em uma rota

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,13 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Drawer.vue"),
   },
+
+  {
+    path: "/cards",
+    name: "Cards",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Cards.vue"),
+  },
 ];
 
 const router = new VueRouter({
