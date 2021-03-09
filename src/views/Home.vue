3a5b0b3c4073269086938ba6e0e74f95fc99d03f
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-03-08 22:13:17 -0300

    criando animacao simples de fade

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <button @click="toggleModal">Open</button>
+
+    <transition name="fade">
+      <div v-if="isOpen" class="modal">
+        <button @click="toggleModal">Close</button>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
+<style scoped>
+.modal {
+  width: 200px;
+  height: 200px;
+  background: #eee;
+  margin: 20px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: .5s ease-out;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
